d0eb098677b0eef53d224d46fec1d984be98730e
Brandon Nichols <brandonaronnichols@gamil.com>
2019-05-22 17:56:22 -0500

    Change challenge 3 in arrays.js to use map instead of for loop

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -73,9 +73,9 @@ console.log(`${lastCar.car_make}, ${lastCar.car_model}`);
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
 let carModels = [];
-for(let i = 0; i < inventory.length; i++){
-    carModels.push(inventory[i].car_model);
-}
+carModels = inventory.map(function(model){
+    return model.car_model;
+});
 
 carModels.sort();
 console.log(carModels);
